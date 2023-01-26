b58914a2206067d00e9d0a8caf4f06926d721b3b
Oleksandr Drahanchuk <draganchuk1991@ukr.net>
2023-01-26 16:22:15 +0200

    dz-2 feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,35 +18,30 @@ countTotalFeedback() {
 countPositiveFeedbackPercentage() {
   return Math.round((this.state.good * 100) / this.countTotalFeedback());
 }
-options = ['Good', 'Neutral', 'Bad'];
+
 onLeaveFeedback = e => {
-  switch (e) {
-    case 'Good':
-      this.setState({ good: this.state.good + 1 });
-      break;
-    case 'Neutral':
-      this.setState({ neutral: this.state.neutral + 1 });
-      break;
-    case 'Bad':
-      this.setState({ bad: this.state.bad + 1 });
-      break;
-    default:
-      return;
-  }
+this.setState({ [e]: this.state[e] + 1 });
+console.log(e,Object.keys(this.state))
+  
 };
 
 render(){
+  
+const{good , neutral ,bad ,}=this.state
   return (
     <Cointeiner>
+
     <Section title="Please leave feedback">
-    <FeedbackOptions options={this.options} onLeaveFeedback={this.onLeaveFeedback}/>
+    <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback}/>
     </Section>
+
     <Section title="Statistick">
+      
       {this.countTotalFeedback()?(
     <Statistics
-    good={this.state.good}
-    neutral={this.state.neutral}
-    bad={this.state.bad}
+    good={good}
+    neutral={neutral}
+    bad={bad}
     total={this.countTotalFeedback()}
     positivePercentage={this.countPositiveFeedbackPercentage()}
    />) :(
@@ -57,8 +52,4 @@ render(){
   }
   export default App
 
-  //class App extends React.Component  ==== щоб додати клас бо в класі можна добавити динаміку , тому клас повинен розширяти react тому добавляєм в кінці extends React.Component
-// в цьому класі є рендер цього класу render ()
-// this - це силка на наш клас App дуже важливо коли ми створюєм клас в реакт компонент то this дає доступ до всьог тобто this.state===доступ до обекта state , this.option поверне масив обєктів і так далі тобто в цьому класі щоб взяти якісь дані все потрібно робити через this
-//Об'єкт-стану state – це властивість класу, яка не повинна безпосередньо змінюватися розробником. Це обовязково обєкт 
-// щоб обновити state можна тільки методом this.setState але змінить не динамічно тобто нприклад якшо в state{good:2}  а ми зробили метод що при кліку на якусь кнопку воно повино збільшуватися на 1  onLeaveFeedback=()=>{ this.setState({good:+1})} то воно перезипише  state{good:3}  але при наступному клікові нічого не зміниться томущо цей метод не запоминає теперішній стан state , щоб змінити динамічно то ми повині повернути фунцкію
+  // options={Object.keys(this.state)} ми передали назви нашого стейту це те саме що ['good', 'neutral', 'bad'] маасив  потім ми перебрали в li і розрендерили
